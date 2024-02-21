5c032177879e4a5d639ecd54564644f6982328c0
Bulat Zamaldinov <b.zamaldinov@yandex.ru>
2024-02-21 14:10:41 +0300

    test tasks completed (fixed task1)

--- task1/task1.py
+++ task1/task1.py
@@ -1,10 +1,10 @@
 import sys
 
 
-def collect_result():
+def collect_result(list_len, step):
     start = 0
     while True:
-        end_index = (m + start) % n - 1
+        end_index = (step + start) % list_len - 1
         val = num_list[end_index]
         if val == 1:
             break
@@ -12,12 +12,12 @@ def collect_result():
         start = end_index
 
 
-argiment_list = sys.argv[1:]
-n = argiment_list[0]
-m = argiment_list[1]
+argument_list = sys.argv[1:]
+n = int(argument_list[0])
+m = int(argument_list[1])
 result = []
 num_list = list(range(1, n + 1))
 result.append(num_list[0])
 
-collect_result()
+collect_result(n, m)
 print(f'Полученный путь: {result}')
